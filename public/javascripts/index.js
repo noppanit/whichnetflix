3c27b23eb320a17b2efe0960bc1399c487bbf623
Noppanit Charassinvichai <noppanit.c@gmail.com>
2014-07-29 13:17:33 +0300

    Added knockout, underscore, jquery and Bacon to display movie titles based on keyword search

---
+++ public/javascripts/index.js
@@ -0,0 +1,31 @@
+
+var movies = ko.observableArray([]);
+
+var MovieModel = function() {
+    return {
+        movies : movies
+    };
+};
+
+var query = $('#q').asEventStream('keyup').throttle(200).map(function(event) {
+    return $(event.target).val();
+}).toProperty('');
+
+queryRequests = query.changes().map(function(query) {
+    return { url: "/search?q=" + query }
+});
+
+queryRequests.flatMapLatest(function(request) {
+    return Bacon.fromPromise($.ajax({
+            url : request.url,
+            method : 'GET',
+            dataType : 'json'
+        }
+    ));
+}).onValue(function(returnedMovies) {
+    movies([]);
+    movies((_.flatten(returnedMovies)));
+});
+
+
+ko.applyBindings(new MovieModel());
